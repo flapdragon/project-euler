83804dc0cfdf90ef27624e5f2231aa94ba399ce5
David Dean <vermicious.knid.d@gmail.com>
2016-07-14 23:05:22 -0700

    fixes comments. adds comments for my thinking on functions.

--- problem6/app.js
+++ problem6/app.js
@@ -4,7 +4,7 @@
 // The sum of the squares of the first ten natural numbers is,
 // 12 + 22 + ... + 102 = 385
 // The square of the sum of the first ten natural numbers is,
-// (1 + 2 + ... + 10)2 = 552 = 3025
+// (1 + 2 + ... + 10)2 = 55 = 3025
 // Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
 // Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
@@ -12,6 +12,7 @@
 
 var limit = 100;
 
+// loop method
 function getSumOfSquares(val) {
     var sum = 0;
     for (var i=0; i <= limit; i++) {
@@ -20,6 +21,7 @@ function getSumOfSquares(val) {
     return sum;
 }
 
+// loop method
 function getSquareOfSums(val) {
     var sum = 0;
     for (var i=0; i <= limit; i++) {
@@ -28,6 +30,7 @@ function getSquareOfSums(val) {
     return sum * sum;
 }
 
+// do both at once with one loop
 function getDifference(val) {
     var sum = 0,
         sumSq = 0;
@@ -39,11 +42,13 @@ function getDifference(val) {
     return (sumSq * sumSq) - sum;
 }
 
+// sum of integers function (borrowed from site)
 function sumIntegers(val) {
     // sum(n) = n(n + 1)=2;
     return val * (val + 1) / 2;
 }
 
+// square of sum function (borrowed from site)
 function squareSum(val) {
     // sum sq = (2limit + 1)(limit + 1)limit=6
     return (2 * val + 1) * (val + 1) * val / 6;
