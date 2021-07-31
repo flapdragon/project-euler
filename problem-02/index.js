92637f75766df0fe33a54a599ac58c74cf9c36d9
David Dean <vermicious.knid.d@gmail.com>
2021-07-31 15:06:13 -0700

    Created my best solution using F(n)=4*F(n-3)+F(n-6) and iterating only over even numbers. Added WIL. Made text changes on problem-01 which is not best practice but fuck it.

--- problem-02/index.js
+++ problem-02/index.js
@@ -7,6 +7,8 @@
 
 // My original solution from my first pass years ago. Iteration, brute force. Knock knock, Fib!
 // Originally I used an array, probably now I would only keep track of the last 2 values as separate int variables to save space and array processing.
+// I'm going to give my good old brute force method 3rd place as it was included in Euler's own solutions. Good old Brute Force. Nothing beats Brute Force.
+// 🥉 Third place
 function bruteForce() {
   var s = 2,
     i = 2,
@@ -24,7 +26,7 @@ function bruteForce() {
   return s;
 }
 
-// Ok so apparently every 3rd fibonacci number is even because the preceding 2 are odd and 2 odds added together are even 🤯🤯
+// Ok so apparently every 3rd Fibonacci number is even because the preceding 2 are odd and 2 odds added together are even 🤯🤯
 // This solution is based on this information and Project Euler's second solution. Still iterating.
 // 🥈 Second place
 function onlyEvenFans() {
@@ -42,12 +44,36 @@ function onlyEvenFans() {
   return sum;
 }
 
+// The best solution is to avoid testing for even numbers in the Fibonacci sequence and use math to find where the evens are hiding.
+// In fact using Euler's theorem, you can loop over just the even numbers. I don't know of a way to do this without iterating.
+// For once I have created the best solution myself but only by using the formula from Euler.
+// This one performs best and scales better than the other solutions the higher you go.
+// 🥇 1st place
+function evenFibonaccis() { // have feelings
+  const limit = 4000000;
+  let sum = 10, // Adding the first two even numbers
+    a = 2,
+    b = 8;
+  let c = 4 * b + a;
+  while (c < limit) {
+    sum += c;
+    a = b;
+    b = c;
+    c = 4 * b + a;
+  }
+  return sum;
+}
+
 // Tests
+console.log(bruteForce());
 console.log(onlyEvenFans());
+console.log(evenFibonaccis());
 
 // What I learned
-// Every 3rd fibonacci number is even because the preceding 2 numbers are odd and 2 odds added together are even. I mean it has to be this way.
+// Every 3rd Fibonacci number is even because the preceding 2 numbers are odd and 2 odds added together are even. I mean it has to be this way.
+// Starting at 1, not 0. So, 1, 1, 2, 3, 5, 8, 13, 21, 34 ...
 // 1. odd + even = odd, 2. even + odd = odd, 3. odd + odd = even. 🤯 How did I never notice that?
-// 
+// I also learned the formula for getting at this pattern of even only Fibonacci numbers: F(n) = 4*F(n-3) + F(n-6), which itself is based on Fibonacci, E(n) = E(n-1) + E(n-2).
 
 // Notes
+// There were no notes.
