b9252e62b20ccce8b22675f66e8144ae81b5d640
David Dean <vermicious.knid.d@gmail.com>
2021-11-03 15:35:35 -0700

    Update HTML, rename folder, rename original, add index.js, add README.

---
+++ problems/problems-0001-0050/problem-09/index.js
@@ -0,0 +1,21 @@
+// 🧙 PROBLEM
+// https://projecteuler.net/problem=9
+// Special Pythagorean triplet
+// Problem 9
+// A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+// a² + b² = c²
+// For example, 3² + 4² = 9 + 16 = 25 = 5².
+// There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+// Find the product abc.
+
+// SOLUTION 1
+// Solution 1 Tests
+
+
+// 🚀 PERFORMANCE
+
+
+// 🧠 WIL WHAT I LEARNED
+
+
+// 📘 NOTES
