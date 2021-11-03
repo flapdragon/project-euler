be6d28ee964cb35276a9327f619c94ba585d75c7
David Dean <vermicious.knid.d@gmail.com>
2021-11-03 16:34:24 -0700

    Add solution 1, straightforward.

--- problems/problems-0001-0050/problem-09/index.js
+++ problems/problems-0001-0050/problem-09/index.js
@@ -9,7 +9,23 @@
 // Find the product abc.
 
 // SOLUTION 1
+// Project Euler's straightforward approach
+// This is an ieterative approach and runs on average about 1.5 seconds at s = 1000.
+const straighforward = function(s) {
+  const lenA = Math.floor((s - 3) / 3); // This is not an integer but it doesn't seem to effect loop performance.
+  for (let a = 3; a < lenA; a++) {
+    let lenB = (s - 1 - a) / 2;
+    for (let b = (a+1); b < lenB; b++) {
+      c = s - a - b;
+      if (c * c === a * a + b * b) {
+        return (a * b * c);
+      }
+    }
+  }
+};
+
 // Solution 1 Tests
+console.log(straighforward(1000));
 
 
 // 🚀 PERFORMANCE
